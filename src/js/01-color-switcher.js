dd63faf7d3070d213dfb45e89d89900dd1704ede
Max Semena <me.semena@gmail.com>
2022-12-24 17:43:19 +0200

    Added project files. Added Task 01.

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,35 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+}
+
+const SWITCH_DELAY = 1000;
+
+let isSwitching = false;
+let switchTimerId = null;
+
+refs.startBtn.addEventListener('click', onStart); 
+refs.stopBtn.addEventListener('click', onStop);
+
+
+function onStart() {
+  if (isSwitching) {
+    return;
+  } else {
+    switchTimerId = setInterval(switchColor, SWITCH_DELAY);
+    isSwitching = true;
+  }
+}
+
+function onStop() {
+  clearInterval(switchTimerId);
+  isSwitching = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function switchColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
